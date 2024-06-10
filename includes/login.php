426a2f3e7d795a931ccc400716cb7151d41dd75f
Sunil Kumar <sunil-k001@webreinvent.com>
2024-06-10 18:08:52 +0530

    Verifying admin username and password entered by user with the login credentials, available in the database admin table.

---
+++ includes/login.php
@@ -0,0 +1,30 @@
+<?php
+//start session
+session_start();
+
+include_once('function.php');
+
+$user = new User();
+
+if(isset($_POST['login'])){
+    echo $username = $user->escape_string($_POST['username']);
+    echo $password = $user->escape_string($_POST['password']);
+
+    die();
+
+    $auth = $user->check_login($username, $password);
+
+    if(!$auth){
+        $_SESSION['message'] = 'Invalid username or password';
+        header('location:../login.php');
+    }
+    else{
+        $_SESSION['user'] = $auth;
+        header('location:../index.php');
+    }
+}
+else{
+    $_SESSION['message'] = 'You need to login first';
+    header('location:../login.php');
+}
+?>
