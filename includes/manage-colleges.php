d2729281dbc481bb2cac84441ba5252a7815f90f
Sunil Kumar <sunil-k001@webreinvent.com>
2024-06-12 10:10:21 +0530

    Confirm box on delete of college
    A confirm box will appear by clicking on the college delete btn, saying "Are you sure, You want to delete this College?"

--- includes/manage-colleges.php
+++ includes/manage-colleges.php
@@ -60,7 +60,7 @@
                                             <td class="align-middle"><?php echo $row['address']; ?></td>
                                             <td class="align-middle"><?php echo $row['status']; ?></td>
                                             <td class="align-middle"><a href="#" class="text-light  bg-success px-2 rounded p-1 "><i class="bi bi-pencil-square"></i> </a></td>
-                                            <td class="align-middle"><a href="includes/function.php?delete_college=<?=$college_id?>" class="text-light bg-danger px-2 rounded p-1 "><i class="bi bi-trash"></i> </a></td>
+                                            <td class="align-middle"><a href="includes/function.php?delete_college=<?=$college_id?>" class="text-light bg-danger px-2 rounded p-1 " onclick="return  confirm('Are you sure, You want to delete this College? ')"><i class="bi bi-trash"></i> </a></td>
                                         </tr>
 
                                         <?php
